af5d56b3185eb17f062e53d3a55304ed77460673
Ben Trainum <enderwing1@gmail.com>
2017-11-08 00:59:12 -0500

    decided to comment my code (the code is short but it's good practice)

--- image_scanner.py
+++ image_scanner.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-
 # ^^ just in case I ever run this from command line
 
+
+#imports
 from PIL import Image
 
+#open image file
 img = Image.open('pineapple.jpeg','r')
 
+#get pixel metadata
 pix_val = list(img.getdata())
 input("done")
-#print(pix_val)
+#print(pix_val)      #<-- massive print statement 
 
+#add all pixels that aren't white to a new list
 not_white = []
 summ = 0
 for pixel in pix_val:
@@ -18,5 +22,4 @@ for pixel in pix_val:
         summ += num
     if summ != 765:
         not_white.append(pixel)
-
 print(not_white)
