15d67c4abf6b345b71321c661d2d8e1a29719c89
Ben Trainum <enderwing1@gmail.com>
2017-11-08 22:49:43 -0500

    image_scanner now asks for file path rather than assuming you also have a file named pineapple.jpeg - program still isn't useful tho

--- image_scanner.py
+++ image_scanner.py
@@ -4,14 +4,17 @@
 
 #imports
 from PIL import Image
+import random
 
 #open image file
-img = Image.open('pineapple.jpeg','r')
+img_file_path = input("image path (from current directory, exclude file extension)\n>>> ")
+img_file_ext = input("File extnsion of the image (png, jpeg, etc.\n>>> ")
+img = Image.open(img_file_path+'.'+img_file_ext,'r')
 
 #get pixel metadata
 pix_val = list(img.getdata())
 input("done")
-#print(pix_val)      #<-- massive print statement 
+#print(pix_val)                 #<-- massive print statement 
 
 #add all pixels that aren't white to a new list
 not_white = []
@@ -22,4 +25,14 @@ for pixel in pix_val:
         summ += num
     if summ != 765:
         not_white.append(pixel)
-print(not_white)
+#print(not_white)                #<-- takes ages to print
+
+new_img = Image.new(img.mode, img.size)
+new_img.putdata(not_white)
+new_img.save('image_output1.' + img_file_ext)
+
+rand_pix = pix_val
+random.shuffle(rand_pix)
+new_img = Image.new(img.mode, img.size)
+new_img.putdata(rand_pix)
+new_img.save('image_output2.' + img_file_ext)
